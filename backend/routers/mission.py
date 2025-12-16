d7f43963b52a317fa17cd67598f7a9db8d94525f
parkjieon1 <qkrdlsp1518@gmail.com>
2025-12-16 17:56:38 +0900

    Delete backend directory

--- backend/routers/mission.py
+++
@@ -1,8 +0,0 @@
-# backend/routers/mission.py
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/ping")
-def ping_mission():
-    return {"message": "mission ok"}
--- backend/routers/user.py
+++
@@ -1,8 +0,0 @@
-# backend/routers/user.py
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/ping")
-def ping_user():
-    return {"message": "user router ok"}
